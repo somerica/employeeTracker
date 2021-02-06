e997dad0ca7314718536edd2fbc173f9a929ee3b
Somer Jackson <somer.jackson@gmail.com>
2021-02-05 22:23:33 -0500

    Add server.js with console table

---
+++ server.js
@@ -0,0 +1,22 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var cTable = require("console.table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "spootie1",
+    database: "employee_DB"
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log('Tech');
+  }); 
