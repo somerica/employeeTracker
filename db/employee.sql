a13a237f13ef0a81dc111595e7b50e6b803195d4
Somer Jackson <somer.jackson@gmail.com>
2021-02-05 22:10:24 -0500

    Add tables for the database

--- db/employee.sql
+++ db/employee.sql
@@ -3,3 +3,26 @@ DROP DATABASE IF EXISTS employee_DB;
 CREATE DATABASE employee_DB;
 
 USE employee_DB;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  department_id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (department_id),
+); 
+
+CREATE TABLE employee (
+    employee_id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (employee_id),
+)
+
+CREATE TABLE role (
+    role_id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NULL,
+    salary DECIMAL(10,4),
+    department_id INT,
+)
