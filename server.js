148bd8128b40da13a193323707c484ed5fe66300
Somer Jackson <somer.jackson@gmail.com>
2021-02-05 22:44:15 -0500

    Log out employees with inquirer

--- server.js
+++ server.js
@@ -18,5 +18,35 @@ var connection = mysql.createConnection({
 
   connection.connect(function(err) {
     if (err) throw err;
-    console.log('Tech');
-  }); 
+    runSearch();
+  });
+
+  function runSearch() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: [
+          "View all employees",
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "View all employees":
+          employeeSearch();
+          break;
+        }
+      });
+  }
+
+  // Search the employee table 
+  function employeeSearch() {
+    connection.query("SELECT employee.employee_id, employee.first_name, employee.last_name, department.name, role.title, role.salary FROM ((department INNER JOIN role ON department.department_id=role.department_id) INNER JOIN employee ON role.role_id = employee.role_id)", function(err, res) {
+        if (err) throw err;
+
+        // Log all results of the SELECT statement
+        console.table(res);
+        runSearch();
+      });
+  } 
